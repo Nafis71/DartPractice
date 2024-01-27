9b50460b9bf359c06b90ddd82bdb6a934bfb617f
Nafis71 <nhtonmoy2@gmail.com>
2024-01-27 20:59:54 +0600

    List in dart

--- bin/list.dart
+++ bin/list.dart
@@ -1,16 +1,19 @@
-var city = ["Dhaka","Mymenshingh","Rajshahi","Barishal"];
+var city = ["Dhaka", "Mymenshingh", "Rajshahi", "Barishal"];
 
-void main(){
+void main() {
   //printing all elements from city list
-  print(city);
+  print("All values: "+city.toString());
 
   //printing specific element from the list by it's index
-  print(city[2]);
+  print("Specific Value : "+city[2].toString());
 
   //printing a set of ranged elements from the list by it's index range
   var start = 0; //starting from 0 index
-  var end = 2;  //ending at 2 index;
+  var end = 2; //ending at 2 index;
 
   //getting 2 values from the list which 0 and 1 index.
-  print(city.getRange(start, end));
+  print("Ranged Values : " + city.getRange(start, end).toString());
+
+  //getting the length of the list
+  print('The length of the list: ' + city.length.toString());
 }
