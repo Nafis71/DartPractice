a4dc94a74f118bd045de5334e420ef0c6f2350f7
Nafis71 <nhtonmoy2@gmail.com>
2024-01-27 20:49:46 +0600

    String and boolean in dart

---
+++ bin/StringAndBoolean.dart
@@ -0,0 +1,13 @@
+var name = 'My name is Nafis Hasan Tonmoy';
+var countryName = "Bangladesh";
+var isBangladeshi = true;
+
+
+void main(){
+  if(isBangladeshi){
+    print(name);
+    print(countryName);
+  } else{
+    print("You are not bangladeshi");
+  }
+}
