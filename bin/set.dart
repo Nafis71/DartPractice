5310331a95ad4bd778674788ae72112c88372aa9
Nafis71 <nhtonmoy2@gmail.com>
2024-01-28 01:11:25 +0600

    list in dart

---
+++ bin/set.dart
@@ -0,0 +1,17 @@
+var myCitySet = <String> {'Dhaka','Barishal'};
+
+main(){
+
+  /*
+  Set is the unordered collection of the different values of the same type
+  It has much functionality, which is the same as array, but it is unordered,
+  Set doesn't allow storing the duplicate values.
+  Set must contain unique values
+   */
+  //printing whole set
+  print(myCitySet);
+
+  // to add elements in the set
+  myCitySet.add("Jamalpur");
+  print(myCitySet);
+}
