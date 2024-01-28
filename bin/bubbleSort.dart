1e61423de4371f2b58c0e57f9a465a198e80618b
Nafis71 <nhtonmoy2@gmail.com>
2024-01-28 22:53:42 +0600

    Bubble Sort in dart

---
+++ bin/bubbleSort.dart
@@ -0,0 +1,23 @@
+import 'dart:ffi';
+
+var array = [1, 5, 9, 10, 2, 4, 3];
+
+List<int> bubbleSort(List<int> array) {
+  for (int i = 0; i < array.length; i++) {
+    for (int j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+main() {
+  print("Before Sorted: ${array}");
+  print("After Sorted: ${bubbleSort(array)}");
+
+  //it has a total time complexity of O(n-square);
+}
