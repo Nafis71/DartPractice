03c18fe1332679fc395faaefdd5c59c59b154320
Nafis71 <nhtonmoy2@gmail.com>
2024-02-17 00:48:44 +0600

    Get second max from the list

---
+++ bin/secondMaxValue.dart
@@ -0,0 +1,18 @@
+int getSecondMax(List<int> listOfNumbers){
+  int max = listOfNumbers[0];
+  int secondMax = max;
+  for(int value in listOfNumbers){
+    if(value > max){
+      secondMax = max;
+      max = value;
+    } else if(value > secondMax && value != max){ // "value != max" is for checking duplicate values in the list.
+      secondMax = value;
+    }
+  } //time complexity O(n);
+  return secondMax;
+}
+
+void main(){
+  List<int> listOfNumbers = [13,34,2,34,33,33,1,35];
+  print("Second Maximum Number is - ${getSecondMax(listOfNumbers)}");
+}
