62af880fe2052fa8447a8899b97e397079c4e999
Nafis71 <nhtonmoy2@gmail.com>
2024-02-16 23:30:20 +0600

    Reversing a list with built in and manual function with complexity of O(n)

--- bin/reverseList.dart
+++ bin/reverseList.dart
@@ -11,7 +11,12 @@ List<int> reverseList(List<int> listOfNumbers) {
   return listOfNumbers;
 }
 
+List<int> reverseListWithIterable(List<int> listOfNumbers) {
+  return new List.from(listOfNumbers.reversed);
+}
+
 main() {
   List<int> listOfNumbers = [3, 2, 4, 7, 10, 6, 5];
+  print(reverseListWithIterable(listOfNumbers));
   print(reverseList(listOfNumbers));
 }
