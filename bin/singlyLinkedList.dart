8dd1a96240ccbd93bb9ed7401ebb6852e6cdb3ed
Nafis71 <nhtonmoy2@gmail.com>
2024-02-19 18:26:12 +0600

    Insert first and last element in linked list

--- bin/singlyLinkedList.dart
+++ bin/singlyLinkedList.dart
@@ -1,25 +1,64 @@
-class LinkedList{
+late LinkedList? head = null;
+late LinkedList? tail = null;
+
+class LinkedList {
   late int _data;
   late LinkedList? _next;
-  LinkedList(int data){
+
+  LinkedList(int data) {
     this._data = data;
     this._next = null;
   }
-  void displayData(){
-    LinkedList? current = this;
-    while(current != null){
-      print(current._data);
-      current = current._next;
-    }
+}
+
+void displayData() {
+  if(head == null){
+    print("List is empty");
+  }
+  LinkedList? current = head;
+  while (current != null) {
+    print(current._data);
+    current = current._next;
   }
 }
-main(){
-  LinkedList head = LinkedList(11);
-  LinkedList second = LinkedList(15);
-  LinkedList third = LinkedList(18);
-  LinkedList fourth =LinkedList(30);
-  head._next = second;
-  second._next = third;
-  third._next = fourth;
-  head.displayData();
+
+int length() {
+  if(head == null){
+    return 0;
+  }
+  int count = 0;
+  LinkedList? current = head;
+  while (current != null) {
+    count++;
+    current = current._next;
+  }
+  return count;
+}
+
+void insertFirst(int value){
+  LinkedList newNode = LinkedList(value);
+  newNode._next = head;
+  head = newNode;
+  if(tail == null){
+    tail = newNode;
+  }
+}
+
+void insertLast(int value){
+  LinkedList newNode = LinkedList(value);
+  if(head == null){
+    head = newNode;
+    tail = newNode;
+  }else{
+    tail!._next = newNode;
+    tail = newNode;
+  }
+}
+
+
+main() {
+  insertFirst(5);
+  insertLast(6);
+  displayData();
+  print("Size of the linked list: ${length()}");
 }
