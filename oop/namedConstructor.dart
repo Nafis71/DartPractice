b3d8f55521a80ad353c7208dff0e02fd10b29e1c
Nafis71 <nhtonmoy2@gmail.com>
2024-02-21 00:03:22 +0600

    added named constructor

---
+++ oop/namedConstructor.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+class Person {
+  late String? name;
+  late int age;
+  late int salary;
+
+  Person([this.age = 0, this.name = null, this.salary = 0]) {
+    //this is default constructor;
+  }
+
+  Person.second(String name, int age, [this.salary = 0]) {
+    this.name = name;
+    this.age = age;
+  }
+
+  String? get getName {
+    return name;
+  }
+
+  int get getAge {
+    return age;
+  }
+
+  int get getSalary {
+    return salary;
+  }
+
+  set setSalary(int salary) {
+    this.salary = salary;
+  }
+}
+
+main() {
+  print("Enter Person's Name: ");
+  String name = stdin.readLineSync()!;
+  print("Enter Person's Age : ");
+  int age = int.tryParse(stdin.readLineSync()!) ?? 0;
+  Person secondPerson = Person.second(name, age);
+  print("Enter Person's Salary: ");
+  secondPerson.setSalary = int.tryParse(stdin.readLineSync()!) ?? 0;
+  print("Person's Name: ${secondPerson.getName}");
+  print("Person's Age : ${secondPerson.getAge}");
+  print("Person's Salary: ${secondPerson.getSalary}");
+}
