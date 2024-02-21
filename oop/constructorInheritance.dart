c99b46385835b1d883289723f136597e85e9cdfe
Nafis71 <nhtonmoy2@gmail.com>
2024-02-21 21:38:20 +0600

    constructor Inheritance

--- oop/constructorInheritance.dart
+++ oop/constructorInheritance.dart
@@ -1,26 +1,54 @@
-class Laptop{  //parent class
+class Laptop {
+  //parent class
   int price;
   String brand;
-  Laptop(this.price, this.brand);
+  String? _operatingSystem;
+
+  Laptop(this.price, this.brand); //positional parameter
+
+                      //named parameter
+  Laptop.secondConst({required this.price, required this.brand, required String operatingSystem}){
+    this._operatingSystem = operatingSystem;
+  }
+
+  String get getOperatingSystem {
+    if (_operatingSystem != null) {
+      return _operatingSystem!;
+    }
+    return "No operating system installed";
+  }
 }
-class MACBook extends Laptop{  //child class
+
+class MACBook extends Laptop {
+  //child class
   String color;
+
   //two way to achieve same results
 
   // MACBook(super.price, super.brand){
-  //   print("Mac book constructor");
   // }
-  MACBook(int price, String brand,this.color): super(price,brand);
+  MACBook(int price, String brand, this.color) : super(price, brand);
+
+  MACBook.secondConst(
+      int price, String brand, this.color, String operatingSystem)
+      : super.secondConst(
+            price: price, brand: brand, operatingSystem: operatingSystem);
+
   //this one is clean and preferable to me.
 
-  void display(){
+  void display() {
     print("Laptop price is ${super.price}");
     print("Laptop brand is ${super.brand}");
     print("Laptop color is ${this.color}");
+    print("Laptop OS is ${super.getOperatingSystem}");
   }
 }
-main(){
-  MACBook macBook = MACBook(100,"Apple", "Red");
+
+main() {
+  MACBook macBook = MACBook(100, "Apple", "Red");
+  MACBook secondMacBook = MACBook.secondConst(200, "Apple", "Blue", "Mac");
+  secondMacBook.display();
   //here constructor of parent class is called before the child class
+  print("\n");
   macBook.display();
 }
