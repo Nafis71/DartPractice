776f0328ca3b2bdfc2522660ce8548cbac685a14
Nafis71 <nhtonmoy2@gmail.com>
2024-02-23 02:47:40 +0600

    dart switch added

---
+++ bin/switch.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+main(){
+  int n = int.tryParse(stdin.readLineSync()!) ?? 0;
+  switch(n){
+    case 1:
+      print("1st case is true");
+      break;
+    case 2:
+      print("2nd case is true");
+      break;
+    case 3:
+      print("3rd case is true");
+      break;
+    default :
+      print("Wrong Input");
+      break;
+  }
+}
